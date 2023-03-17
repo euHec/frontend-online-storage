9b6526bc0ef7889ef4a80252ac0c50102ca1180c
euHec <hectorsouzass@gmail.com>
2023-03-17 18:42:39 -0300

    Implementa parcialmente a função que busca e retorna os produtos na API

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends Component {
   state = {
     search: '',
-    // products: [],
+    products: [],
   };
 
+  // Função que cuida do preenchimento do atributo search no state
   onInputSearch = ({ target }) => {
     const { value } = target;
     this.setState({ search: value });
   };
 
-  render() {
+  // Função que é aciona uma chamada de API para listar os produtos
+  onSearchProduct = async (event) => {
     const { search } = this.state;
+    event.preventDefault();
+    const response = await getProductsFromCategoryAndQuery(search);
+    const { results } = response;
+    console.log(results);
+    this.setState({ products: results });
+  };
+
+  render() {
+    const { search, products } = this.state;
+    const UNDEFINED = <span>Nenhum produto foi encontrato</span>;
+    console.log(products);
     return (
       <>
         <div>
@@ -35,6 +49,11 @@ class Home extends Component {
                 Digite algum termo de pesquisa ou escolha uma categoria.
               </span>
             )}
+          {/* {
+            products === undefined ? UNDEFINED : (
+              <span> produtos </span>
+            )
+          } */}
         </div>
       </>
     );
