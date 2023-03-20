092abbdcce9e5590ddddb2ee18b341198052fbdd
andhuangg <anderson.huang93@gmail.com>
2023-03-20 19:09:36 -0300

    req 07 correcoes finais

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Card.css';
+import { Link } from 'react-router-dom';
 
 class Card extends Component {
   render() {
     const { details } = this.props;
-    const { title, thumbnail, price } = details;
+    const { title, thumbnail, price, id } = details;
     return (
       <div data-testid="product" className="product">
-        {/* < Link to={`/product/${id}`}/> */}
-        <div>
-          <span>{title}</span>
-        </div>
-        <div>
-          <img src={ thumbnail } alt={ title } />
-        </div>
-        <div>
-          <span>{price}</span>
-        </div>
+        <Link to={ `/product/${id}` } data-testid="product-detail-link">
+          <div>
+            <span>{title}</span>
+          </div>
+          <div>
+            <img src={ thumbnail } alt={ title } />
+          </div>
+          <div>
+            <span>{ price }</span>
+          </div>
+        </Link>
       </div>
     );
   }
