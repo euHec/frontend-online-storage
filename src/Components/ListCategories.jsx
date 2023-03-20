e610b1ff63d97916f235c6141edd464198148634
euHec <hectorsouzass@gmail.com>
2023-03-20 17:26:00 -0300

    Implementa proptypes em ListCategories

--- src/Components/ListCategories.jsx
+++ src/Components/ListCategories.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class ListCategories extends Component {
   render() {
@@ -18,5 +19,10 @@ class ListCategories extends Component {
     );
   }
 }
+
+ListCategories.propTypes = {
+  listOfCategories: PropTypes.string.isRequired,
+  productsByCategory: PropTypes.func.isRequired,
+};
 
 export default ListCategories;
