5d8b84d4733a2196b8c519c908bcbbdda9a7f588
euHec <hectorsouzass@gmail.com>
2023-05-28 20:23:49 -0300

    Renderiza os produtos comprados na tela de checkout

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -1,9 +1,48 @@
+import PropTypes from 'prop-types';
 import { Component } from 'react';
 
 export default class Checkout extends Component {
+  state = {
+    products: [],
+  };
+
+  componentDidMount() {
+    const storage = JSON.parse(localStorage.getItem('cart')) || [];
+    this.setState(() => ({ products: storage }));
+  }
+
   render() {
+    const { history } = this.props;
+    const { products } = this.state;
     return (
-      <p>Checkout</p>
+      <>
+        <button onClick={ () => history.push('/cart') }>
+          retornar
+        </button>
+        <div>
+          <div>
+            <h1>revise seus produtos</h1>
+            <div>
+              {
+                products.map((product, index) => (
+                  <div key={ index }>
+                    <h3 data-testid="shopping-cart-product-name">{ product.name }</h3>
+                    <img src={ product.image } alt={ product.name } />
+                    <h3>{`Quantidade ${product.qt}`}</h3>
+                    <h3>{`R$ ${product.value * product.qt}`}</h3>
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 }
+
+Checkout.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
